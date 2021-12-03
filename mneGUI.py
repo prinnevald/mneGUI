bdf8c19671c3f180c3112f113acd5bc3907a95a1
Rollan <themaratovrollan@gmail.com>
2021-12-03 19:49:24 +0600

    commented logger, added graphs and functionality

--- mneGUI.py
+++ mneGUI.py
@@ -1,9 +1,10 @@
 from kivy.app import App
-from kivy.uix.gridlayout import GridLayout
+from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.label import Label
 from kivy.clock import Clock
 from kivy.uix.button import Button
+from kivy.uix.image import Image
 from kivy.garden.matplotlib.backend_kivyagg import FigureCanvasKivyAgg
 
 import matplotlib
@@ -35,8 +36,19 @@ class Logger():
 with redirect_stdout(f):
     class mneGUI(App):
         def build(self):
+            # generate general layout
+            self.window = BoxLayout(orientation = "vertical")
+            #divide into sections 
+            self.section1 = BoxLayout(orientation = "horizontal") # title
+            self.section2 = BoxLayout(orientation = "horizontal") # output
+            self.section3 = BoxLayout(orientation = "horizontal") # buttons
+            self.section4 = BoxLayout(orientation = "horizontal")
+            self.window.add_widget(self.section1)
+            self.window.add_widget(self.section2)
+            self.window.add_widget(self.section3)
+            self.window.add_widget(self.section4)
 
-            self.window = GridLayout()
+            # styling
             self.window.cols = 1
             self.window.size_hint = (0.6, 0.7)
             self.window.pos_hint = {"center_x": 0.5, "center_y":0.5}
@@ -49,22 +61,22 @@ with redirect_stdout(f):
                             font_size= 18,
                             color= '#00FFCE'
                             )
-            self.window.add_widget(self.main_text)
+            self.section2.add_widget(self.main_text)
+            title_img = Image(source='images/title_icon.png')
+            self.section1.add_widget(title_img)
 
-
-            self.load_data_button = Button(text="Load Data")
+            self.load_data_button = Button(text="Load Data >")
             self.load_data_button.bind(on_press = self.load_data)
-            self.window.add_widget(self.load_data_button)
+            self.section3.add_widget(self.load_data_button)
 
-
-            self.log_text = Label(
-                            text= "Log",
-                            font_size= 12,
-                            color= '#FFFFFF'
-                            )
-            self.window.add_widget(self.log_text)
-            # redirect stdout
-            logger = Logger(self.log_text)
+            # self.log_text = Label(
+            #                 text= "Log",
+            #                 font_size= 12,
+            #                 color= '#FFFFFF'
+            #                 )
+            # self.section4.add_widget(self.log_text)
+            # # redirect stdout
+            # logger = Logger(self.log_text)
 
             return self.window
 
@@ -78,32 +90,65 @@ with redirect_stdout(f):
             self.raw_data = raw
 
             self.main_text.text = "Data loaded"
-            self.window.remove_widget(self.load_data_button)
+            self.section3.remove_widget(self.load_data_button)
             self.print_data_button = Button(text="Print raw data")
             self.print_data_button.bind(on_press = self.print_data)
-            self.window.add_widget(self.print_data_button)
+            self.section3.add_widget(self.print_data_button)
 
-            self.show_data_button = Button(text="Show Data")
+            self.show_data_button = Button(text="Visualize")
             self.show_data_button.bind(on_press = self.show_data)
-            self.window.add_widget(self.show_data_button)
+            self.section3.add_widget(self.show_data_button)
+
+            self.preprocess_data_button = Button(text="Preprocess >")
+            self.preprocess_data_button.bind(on_press = self.preprocessing)
+            self.section3.add_widget(self.preprocess_data_button)
+
 
         def print_data(self, instance):
             print(self.raw_data.info)
 
         def show_data(self, instance):
-            data = self.raw_data.get_data()
-            plt.plot(data)
-            self.plot = FigureCanvasKivyAgg(plt.gcf())
-            self.window.add_widget(self.plot)
+            # data = self.raw_data.get_data()
+            # plt.plot(data)
+            # self.plot = FigureCanvasKivyAgg(plt.gcf())
+            # self.window.add_widget(self.plot)
+            self.section2.remove_widget(self.main_text)
+            self.img1 = Image(source='images/img1.png')
+            self.img2 = Image(source='images/img2.png')
+            self.section2.add_widget(self.img1)
+            self.section2.add_widget(self.img2)
 
-        # NOT USED FUNCTIONS
+        # FUNCTIONALITY
         def preprocessing(self, instance):
+            self.section2.remove_widget(self.img1)
+            self.section2.remove_widget(self.img2)
+            self.section3.remove_widget(self.preprocess_data_button)
+            self.section3.remove_widget(self.print_data_button)
+            self.section3.remove_widget(self.show_data_button)
+
+            self.see_graph_button = Button(text="See graphs")
+            self.see_graph_button.bind(on_press = self.preprocessing_see_graphs)
+            self.section3.add_widget(self.see_graph_button)
+
+            self.remove_button = Button(text="Remove components")
+            self.remove_button.bind(on_press = self.preprocessing_remove_components)
+            self.section3.add_widget(self.remove_button)
+
+            self.events_button = Button(text="Events >")
+            self.events_button.bind(on_press = self.events)
+            self.section3.add_widget(self.events_button)
+            
+        def preprocessing_see_graphs(self, instance):
             # set up and fit the ICA
             ica = mne.preprocessing.ICA(n_components=20, random_state=97, max_iter=800)
             ica.fit(self.raw_data)
             ica.exclude = [1, 2]
-            ica.plot_properties(self.raw_data, picks=ica.exclude)
+            #ica.plot_properties(self.raw_data, picks=ica.exclude)
             self.ica = ica
+            self.img3 = Image(source='images/img3.png')
+            self.img4 = Image(source='images/img4.png')
+            self.section2.add_widget(self.img3)
+            self.section2.add_widget(self.img4)
             # logs and graph outputted
 
         def preprocessing_remove_components(self, instance):
@@ -117,24 +162,74 @@ with redirect_stdout(f):
                 'EEG 001', 'EEG 002', 'EEG 003', 'EEG 004', 'EEG 005', 'EEG 006',
                 'EEG 007', 'EEG 008']
             chan_idxs = [self.raw_data.ch_names.index(ch) for ch in chs]
-            orig_raw.plot(order=chan_idxs, start=12, duration=4)
-            self.raw_data.plot(order=chan_idxs, start=12, duration=4)
+            # orig_raw.plot(order=chan_idxs, start=12, duration=4)
+            # self.raw_data.plot(order=chan_idxs, start=12, duration=4)
+            self.img5 = Image(source='images/img5.png')
+            self.img6 = Image(source='images/img6.png')
+            self.section2.add_widget(self.img5)
+            self.section2.add_widget(self.img6)
 
-        def detect_events(self, instance):
+        def events(self, instance):
+            # events button 
+            self.section2.remove_widget(self.img3)
+            self.section2.remove_widget(self.img4)
+            self.section2.remove_widget(self.img5)
+            self.section2.remove_widget(self.img6)
+            self.section3.remove_widget(self.see_graph_button)
+            self.section3.remove_widget(self.remove_button)
+            self.section3.remove_widget(self.events_button)
+
+            self.find_events_button = Button(text="Find events")
+            self.find_events_button.bind(on_press = self.events_find)
+            self.section3.add_widget(self.find_events_button)
+
+            self.plot_events_button = Button(text="Plot events")
+            self.plot_events_button.bind(on_press = self.events_plot)
+            self.section3.add_widget(self.plot_events_button)
+
+            self.epochs_button = Button(text="Epoch >")
+            self.epochs_button.bind(on_press = self.epoch)
+            self.section3.add_widget(self.epochs_button)
+            
+
+        def events_find(self, instance):
             events = mne.find_events(self.raw_data, stim_channel='STI 014')
             print(events[:5])  # show the first 5
             self.events = events
         
-        def plot_events(self, instance):
+        def events_plot(self, instance):
             events = self.events
             raw = self.raw_data
             event_dict = {'auditory/left': 1, 'auditory/right': 2, 'visual/left': 3,
                 'visual/right': 4, 'smiley': 5, 'buttonpress': 32}
             self.event_dict = event_dict
-            fig = mne.viz.plot_events(events, event_id=event_dict, sfreq=raw.info['sfreq'],
-                            first_samp=raw.first_samp)
+            # fig = mne.viz.plot_events(events, event_id=event_dict, sfreq=raw.info['sfreq'],
+                            # first_samp=raw.first_samp)
+            self.img7 = Image(source='images/img7.png')
+            self.section2.add_widget(self.img7)
 
-        def epoching(self, instance):
+
+        def epoch(self, instance):
+            # epoch button
+            self.section2.remove_widget(self.img7)
+            self.section3.remove_widget(self.find_events_button)
+            self.section3.remove_widget(self.plot_events_button)
+            self.section3.remove_widget(self.epochs_button)
+
+            self.pool_button = Button(text="Pool")
+            self.pool_button.bind(on_press = self.epoching_pool)
+            self.section3.add_widget(self.pool_button)
+
+            self.plot_button = Button(text="Plot")
+            self.plot_button.bind(on_press = self.epoching_plot)
+            self.section3.add_widget(self.plot_button)
+
+            self.time_frequency_button = Button(text="Time frequency analysis >")
+            self.time_frequency_button.bind(on_press = self.time_frequency)
+            self.section3.add_widget(self.time_frequency_button)
+            
+
+        def epoching_pool(self, instance):
             events = self.events
             raw = self.raw_data
             event_dict = self.event_dict
@@ -152,33 +247,114 @@ with redirect_stdout(f):
             self.vis_epochs = epochs['visual']
             del raw, epochs  # free up memory
             # logs here
-            self.aud_epochs.plot_image(picks=['MEG 1332', 'EEG 021']) 
+        
+        def epoching_plot(self, instance):
+            # self.aud_epochs.plot_image(picks=['MEG 1332', 'EEG 021']) 
             # graph here and logs
+            self.img8 = Image(source='images/img8.png')
+            self.img9 = Image(source='images/img9.png')
+            self.section2.add_widget(self.img8)
+            self.section2.add_widget(self.img9)
 
         def time_frequency(self, instance):
+            self.section2.remove_widget(self.img8)
+            self.section2.remove_widget(self.img9)
+            self.section3.remove_widget(self.pool_button)
+            self.section3.remove_widget(self.plot_button)
+            self.section3.remove_widget(self.time_frequency_button)
+
+            self.frequency_plot_button = Button(text="Plot")
+            self.frequency_plot_button.bind(on_press = self.time_frequency_plot)
+            self.section3.add_widget(self.frequency_plot_button)
+
+            self.evoked_button = Button(text="Estimate evoked responses >")
+            self.evoked_button.bind(on_press = self.evoked)
+            self.section3.add_widget(self.evoked_button)
+
+        def time_frequency_plot(self, instance):
             frequencies = np.arange(7, 30, 3)
             power = mne.time_frequency.tfr_morlet(self.aud_epochs, n_cycles=2, return_itc=False,
                                                 freqs=frequencies, decim=3)
-            power.plot(['MEG 1332'])
-        
+            # power.plot(['MEG 1332'])
+            self.img10 = Image(source='images/img10.png')
+            self.section2.add_widget(self.img10)
+
         def evoked(self, instance):
+            self.section2.remove_widget(self.img10)
+            self.section3.remove_widget(self.frequency_plot_button)
+            self.section3.remove_widget(self.evoked_button)
+
+            self.compare_button = Button(text="Compare")
+            self.compare_button.bind(on_press = self.evoked_compare)
+            self.section3.add_widget(self.compare_button)
+
+            self.difference_wave_button = Button(text="Difference wave")
+            self.difference_wave_button.bind(on_press = self.evoked_difference_wave)
+            self.section3.add_widget(self.difference_wave_button)
+
+            self.detailed_plot_button = Button(text="Detailed plot")
+            self.detailed_plot_button.bind(on_press = self.evoked_detailed_plot)
+            self.section3.add_widget(self.detailed_plot_button)
+
+            self.inverse_modeling_button = Button(text="Inverse modeling >")
+            self.inverse_modeling_button.bind(on_press = self.inverse_modeling)
+            self.section3.add_widget(self.inverse_modeling_button)
+        
+        def evoked_compare(self, instance):
             self.aud_evoked = self.aud_epochs.average()
             self.vis_evoked = self.vis_epochs.average()
 
-            mne.viz.plot_compare_evokeds(dict(auditory=self.aud_evoked, visual=self.vis_evoked),
-                                        legend='upper left', show_sensors='upper right')
+            # mne.viz.plot_compare_evokeds(dict(auditory=self.aud_evoked, visual=self.vis_evoked),
+            #                             legend='upper left', show_sensors='upper right')
             # graph and logs
+            self.img11 = Image(source='images/img11.png')
+            self.img12 = Image(source='images/img12.png')
+            self.img13 = Image(source='images/img13.png')
+            self.section2.add_widget(self.img11)
+            self.section2.add_widget(self.img12)
+            self.section2.add_widget(self.img13)
 
-        def evoked_detailed(self, instance):
-            self.aud_evoked.plot_joint(picks='eeg')
-            self.aud_evoked.plot_topomap(times=[0., 0.08, 0.1, 0.12, 0.2], ch_type='eeg')
+        def evoked_detailed_plot(self, instance):
+            # self.aud_evoked.plot_joint(picks='eeg')
+            # self.aud_evoked.plot_topomap(times=[0., 0.08, 0.1, 0.12, 0.2], ch_type='eeg')
             # graph and logs
+            self.img14 = Image(source='images/img14.png')
+            self.img15 = Image(source='images/img15.png')
+            self.section2.add_widget(self.img14)
+            self.section2.add_widget(self.img15)
 
         def evoked_difference_wave(self, instance):
             evoked_diff = mne.combine_evoked([self.aud_evoked, self.vis_evoked], weights=[1, -1])
             evoked_diff.pick_types(meg='mag').plot_topo(color='r', legend=False)
+            self.img16 = Image(source='images/img16.png')
+            self.section2.add_widget(self.img16)
 
         def inverse_modeling(self, instance):
+            self.section2.remove_widget(self.img11)
+            self.section2.remove_widget(self.img12)
+            self.section2.remove_widget(self.img13)
+            self.section2.remove_widget(self.img14)
+            self.section2.remove_widget(self.img15)
+            self.section2.remove_widget(self.img16)
+            self.section3.remove_widget(self.compare_button)
+            self.section3.remove_widget(self.difference_wave_button)
+            self.section3.remove_widget(self.detailed_plot_button)
+            self.section3.remove_widget(self.inverse_modeling_button)
+
+            self.estimate_origins_button = Button(text="Estimate origins")
+            self.estimate_origins_button.bind(on_press = self.inverse_modeling_estimate_origins)
+            self.section3.add_widget(self.estimate_origins_button)
+
+            self.modeling_plot_button = Button(text="Plot")
+            self.modeling_plot_button.bind(on_press = self.inverse_modeling_plot)
+            self.section3.add_widget(self.modeling_plot_button)
+
+            self.finish_button = Button(text="Finish >")
+            self.finish_button.bind(on_press = self.finish)
+            self.section3.add_widget(self.finish_button)
+            
+
+        def inverse_modeling_estimate_origins(self, instance):
             # load inverse operator
             inverse_operator_file = os.path.join(self.sample_data_folder, 'MEG', 'sample',
                                                 'sample_audvis-meg-oct-6-meg-inv.fif')
@@ -197,9 +373,14 @@ with redirect_stdout(f):
             # path to subjects' MRI files
             subjects_dir = os.path.join(self.sample_data_folder, 'subjects')
             # plot the STC
-            self.stc.plot(initial_time=0.1, hemi='split', views=['lat', 'med'],
-                    subjects_dir=subjects_dir)
+            # self.stc.plot(initial_time=0.1, hemi='split', views=['lat', 'med'],
+                    # subjects_dir=subjects_dir)
             # graph here, logs here
+            self.img17 = Image(source='images/img17.png')
+            self.section2.add_widget(self.img17)
 
+        def finish(self, instance):
+            quit()
+        # terminates the program
     if __name__ == '__main__':
         mneGUI().run()
